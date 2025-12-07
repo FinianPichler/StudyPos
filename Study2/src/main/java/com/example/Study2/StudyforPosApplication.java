2c567f3cc21fdc54fc6b5b0ee3c79b2d8f793b93
Finian Pichler <pic210572@spengergasse.at>
2025-12-07 17:23:27 +0100

    Init commit v2

---
+++ Study2/src/main/java/com/example/Study2/StudyforPosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Study2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyforPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyforPosApplication.class, args);
+	}
+
+}
---
+++ Study2/src/test/java/com/example/Study2/StudyforPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Study2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyforPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
